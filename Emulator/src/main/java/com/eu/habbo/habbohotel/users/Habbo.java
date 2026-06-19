e5ce3629e66f9686b1f2c3a271d82509960b64e2
simoleo89 <simoleo89@users.noreply.github.com>
2026-06-19 22:42:13 +0200

    fix(inventory): modernize wardrobe collections

--- Emulator/src/main/java/com/eu/habbo/habbohotel/users/Habbo.java
+++ Emulator/src/main/java/com/eu/habbo/habbohotel/users/Habbo.java
@@ -19,7 +19,7 @@ import com.eu.habbo.plugin.events.users.UserCreditsEvent;
 import com.eu.habbo.plugin.events.users.UserDisconnectEvent;
 import com.eu.habbo.plugin.events.users.UserGetIPAddressEvent;
 import com.eu.habbo.plugin.events.users.UserPointsEvent;
-import gnu.trove.TIntCollection;
+import it.unimi.dsi.fastutil.ints.IntCollection;
 import gnu.trove.map.hash.THashMap;
 import gnu.trove.set.hash.THashSet;
 import org.slf4j.Logger;
@@ -505,7 +505,7 @@ public class Habbo implements Runnable {
     }
 
     public Set<Integer> getForbiddenClothing() {
-        TIntCollection clothingIDs = this.getInventory().getWardrobeComponent().getClothing();
+        IntCollection clothingIDs = this.getInventory().getWardrobeComponent().getClothing();
 
         return Emulator.getGameEnvironment().getCatalogManager().clothing.values().stream()
                 .filter(c -> !clothingIDs.contains(c.id))
